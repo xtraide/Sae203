a4eaf272af23d7790020bbfee3eb3e942f388090
nicolas thieblemont <xxtraide@gmail.com>
2023-05-12 12:34:33 +0200

    Revert "Add files via upload"
    This reverts commit e36473945ae74ca4ebe31cb93dc356ebb5a4352b.

--- utile/html/nav.php
+++ utile/html/nav.php
@@ -1,60 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/css/nav.css">
-    <title>Document</title>
-</head>
-<body>
-<div class="herp">
-        <nav class="navbar">
-
-            <img src="../../assets/ressources/utile/iutLogo.png" alt="" height="65px" class="iut">
-            <ul class="nav-links">
-                <li class="search">
-                    <input type="text" placeholder="Recherche" value="" class="reinput" name="recherche"></li>
-                <li><a href="">RESERVATION</a></li>
-                <li><a href="">A PROPOS DE NOUS</a></li>
-            </ul>
-
-
-
-            <img src="../../assets/ressources/nav/pp.png" alt="photo utilisateur" class="user-pic" onclick="toggleMenu()">
-
-
-            <div class="sub-menu-wrap" id="submenu">
-                <div class="sub-menu">
-                    <div class="user-info">
-                        <img src="../../assets/ressources/nav/pp.png" alt="">
-                        <h3>Nico</h3>
-                    </div>
-
-                    <hr>
-
-                    <a href="" class="sub-menu-link">
-                        <img src="../../assets/ressources/nav/logo_pp.png" alt="">
-                        <p>profile</p>
-                        <span><img src="../../assets/ressources/nav/fleche_droite.png" alt=""></span>
-                    </a>
-                    <a href="" class="sub-menu-link">
-                        <img src="../../assets/ressources/nav/logo_para.png" alt="">
-                        <p>paramètre</p>
-                        <span><img src="../../assets/ressources/nav/fleche_droite.png" alt=""></span>
-                    </a>
-                    <a href="" class="sub-menu-link">
-                        <img src="../../assets/ressources/nav/log_out.jpg" alt="">
-                        <p>deconexion</p>
-                        <span><img src="../../assets/ressources/nav/fleche_droite.png" alt=""></span>
-                    </a>
-                </div>
-            </div>
-        </nav>
-    </div>
-    <script src="../../assets/js/nav.js"></script>
-</body>
-</html>
 <?php
 $array = scandir("../public/");/*, [
     ".",
@@ -70,6 +13,6 @@ $array = scandir("../public/");/*, [
 foreach ($array as $test) {
 ?>
 
-
+    <a href="<?= $test ?>"><?= $test ?></a> <br>
 <?php
 } ?>
