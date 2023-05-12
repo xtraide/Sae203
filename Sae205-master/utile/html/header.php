7db274609f4a523e67950b4a6d374c24c030bf5e
RETR0EDIT <86560394+RETR0EDIT@users.noreply.github.com>
2023-05-12 11:14:45 +0200

    Add files via upload

---
+++ Sae205-master/utile/html/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<title>SAE 203</title>
+</head>
+
+<body>
+	<header>
+		<h2>CA C'EST POUR TOI MON KILLIAN </h2>
+		<?php include_once("nav.php"); ?>
+	</header>
+	<?php
+	include "../utile/link/linkPdo.php";
+	include "../utile/function.php";
+	session_start();
+
+	if (empty($_SESSION['verified'])) {
+
+		if (empty($_COOKIE['id'])) {
+
+			header("Location: login.php");
+	?>
+			<script>
+				alert("vous devez etre connecter pour utiliser cette page vous allez etre rediriger vers la page de connection ")
+			</script>
+
+			<?php
+		} else {
+
+			$result = execute("SELECT * FROM `utilisateur` WHERE id = :id", [
+				"id" => $_COOKIE['id']
+			]);
+			while ($row = $result->fetchAll(PDO::FETCH_ASSOC)) {
+				foreach ($row as $row) {
+
+					if ($row['verified'] != 1) {
+			?>
+						<script>
+							alert("vous devez verifier votre adresse mail  pour utiliser cette page vous allez etre rediriger vers la page de connection ")
+						</script>
+	<?php
+						die;
+					} else {
+
+						$_SESSION['verified'] = 1;
+					}
+				}
+			}
+		}
+	}
---
+++ Sae205-master/utile/link/link.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/*
+function  execute($query){
+    include "../utile/link/config.php";
+    $connection = mysqli_connect($host, $username, $password, $dbname);
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }else {
+        $result = mysqli_query($connection, $query) or exit('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error($connection));
+        return    $result;
+        mysqli_close($connection);
+        mysqli_free_result($result);
+    }
+}
+/*  $result = execute("SELECT * FROM auteur");
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['Nom']."<br>";
+        }
+    } else {
+        echo "No results found.";
+    }s */
