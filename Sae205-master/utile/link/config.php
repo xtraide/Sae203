7f74aade01a9074aebbf7b8fc5917b94e76334f2
nicolas thieblemont <xxtraide@gmail.com>
2023-05-12 12:35:13 +0200

    Revert "Add files via upload"
    This reverts commit 7db274609f4a523e67950b4a6d374c24c030bf5e.

--- Sae205-master/utile/link/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-const username = 'root';
-const password = '';
-const dbname = 'sae203_np';
-const host = "localhost";
--- Sae205-master/utile/link/deco.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-session_unset();
-session_destroy();
-unset($_COOKIE['id']); 
-setcookie('id', null, -1, '/'); 
-header('Location: ../../public/sign-in.php');
-
-exit();
