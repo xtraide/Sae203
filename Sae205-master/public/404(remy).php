d64d6ea9eb0c47fb2f2de2a8d007b34332012ede
RETR0EDIT <86560394+RETR0EDIT@users.noreply.github.com>
2023-05-12 11:16:50 +0200

    Delete Sae205-master directory

--- Sae205-master/public/404(remy).php
+++
@@ -1,9 +0,0 @@
-<?php
-$path = "../utile/";
-include $path . "html/header.php"; ?>
-ERROR 404
-page d'erreur veilleur clique
-<a href="index.php">
-    <h1>LA</h1>
-</a><?php
-    include $path . "html/footer.php";
