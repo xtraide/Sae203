89ed0a3d56688e1dcd20c1bb3c0da14d3e0d3737
RETR0EDIT <86560394+RETR0EDIT@users.noreply.github.com>
2023-05-28 17:05:56 +0200

    Add files via upload

--- utile/html/nav.php
+++ utile/html/nav.php
@@ -7,13 +7,11 @@ if ($result->rowCount() > 0) {
             <div class="hero">
                 <nav class="navbar">
 
-                    <img src="../assets/ressources/utile/navimg/iutLogo.png" alt="" height="65px" class="iut">
+                    <a href="/sae205-master/public/index.php"><img src="../assets/ressources/utile/navimg/iutLogo.png" alt="" height="65px" class="iut"></a>
                     <ul class="nav-links">
-                        <li class="search">
-                            <input type="text" placeholder="Recherche" value="" class="reinput" name="recherche">
-                        </li>
-                        <li><a href="">RESERVATION</a></li>
-                        <li><a href="">A PROPOS DE NOUS</a></li>
+
+                        <li><a href="/sae205-master/public/list-reservation.php">RESERVATION</a></li>
+                        <li><a href="/sae205-master/public/nous.php">A PROPOS DE NOUS</a></li>
                     </ul>
 
                     <img src="../assets/ressources/utile/navimg/pp.png" alt="photo utilisateur" class="user-pic" onclick="toggleMenu()">
@@ -22,10 +20,10 @@ if ($result->rowCount() > 0) {
                         <div class="sub-menu">
                             <div class="user-info">
                                 <img src="../assets/ressources/utile/navimg/pp.jpg" alt="">
-                                <p><?= $row['nom'] . ' ' . $row['prenom']; ?></p>
+                                <p id="name"><?= $row['nom'] . ' ' . $row['prenom']; ?></p>
                             </div>
 
-                            <a href="" class="sub-menu-link">
+                            <a href="/sae205-master/public/compte.php" class="sub-menu-link">
                                 <img src="../assets/ressources/utile/navimg/logo_pp.png" alt="">
                                 <p class="navtexte">profile</p>
                                 <span><img src="../assets/ressources/utile/navimg/fleche_droite.png" alt=""></span>
@@ -35,7 +33,7 @@ if ($result->rowCount() > 0) {
                                 <p class="navtexte">paramètre</p>
                                 <span><img src="../assets/ressources/utile/navimg/fleche_droite.png" alt=""></span>
                             </a>
-                            <a href="" class="sub-menu-link">
+                            <a href="../link/deco.php" class="sub-menu-link">
                                 <img src="../assets/ressources/utile/navimg/log_out.jpg" alt="">
                                 <p class="navtexte">deconexion</p>
                                 <span><img src="../assets/ressources/utile/navimg/fleche_droite.png" alt=""></span>
@@ -50,8 +48,6 @@ if ($result->rowCount() > 0) {
     }
 }
 
-
-
 $array = scandir("../public/");
 
 foreach ($array as $test) {
@@ -59,5 +55,6 @@ foreach ($array as $test) {
 
     <a href="<?= $test ?>"><?= $test ?></a> <br>
 <?php
-} ?>
-<script type="text/javascript" src="../assets/js/nav.js"></script>
+} 
+
+?>
