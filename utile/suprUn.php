749fb64c62029a48d80ab5c762f559094fc7b9eb
nolan <nolandme2003@gmail.com>
2023-05-31 00:17:29 +0200

    tt

--- utile/suprUn.php
+++
@@ -1,7 +0,0 @@
-<?php function suprUnUser(string $idUser)
-{
-    execute("DELETE  FROM `utilisateur` WHERE id = :id", [
-        "id" => $idUser
-    ]);
-    header('Location: ../utile/link/deco.php');
-}
